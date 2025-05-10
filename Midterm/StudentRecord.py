cb6d067cbff5ebe54bf37372888ed7371bda0b27
crux091 <rtcruz3@student.apc.edu.ph>
2025-05-10 10:01:10 +0800

    move it to midterm file

---
+++ Midterm/StudentRecord.py
@@ -0,0 +1,10 @@
+def create_student_record():
+    name = input("Enter student name: ")
+    student_id = input("Enter student ID: ")
+    courses_input = input("Enter courses/strand (separated by commas): ")
+    courses = courses_input.split(",")
+    return (name, student_id, courses)
+
+record = create_student_record()    
+print("Student Record:", record)
+#GROUP 6 ABG
